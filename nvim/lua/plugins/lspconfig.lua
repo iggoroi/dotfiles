607fa31b62a312f1dd13ea43087de072263ccd11
iggoroi <iggoroi.g@gmail.com>
2024-01-16 13:10:36 +0100

    aggiunto wezterm

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,7 +6,6 @@ return {
 		"Mason Lspconfig",
 		"cmp",
 	},
-	event = "BufReadPre *.*",
 	config = function()
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		vim.g.rustaceanvim = {
---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+return {
+	window_background_opacity = 0.7,
+	color_scheme = "Tokyo Night Storm",
+	default_prog = { "pwsh" },
+	window_decorations = "NONE",
+	cursor_blink_rate = 500,
+	default_cursor_style = "BlinkingBlock",
+	cursor_blink_ease_in = "Linear",
+	cursor_blink_ease_out = "Linear",
+	animation_fps = 1,
+	automatically_reload_config = true,
+}
