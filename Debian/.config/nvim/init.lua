f76eed1e0781cd641d2cb4feb45abbfc21e2e804
Ramateca <mataceragiorgio@gmail.com>
2024-02-12 21:03:27 +0100

    debian update & packages list

--- Debian/.config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
---luasnip angular snippets
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/angular" } })
