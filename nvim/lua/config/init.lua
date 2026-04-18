9f638801eaeeaa6e133bdff331525496cbae64cf
Giorgio Matacera <mataceragiorgio@gmail.com>
2026-04-18 15:49:34 +0200

    correc setup lazy befor plugins

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,7 +1,3 @@
-require("config.remaps")
-require("config.set")
-require("config.lsp")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -22,3 +18,8 @@ require("lazy").setup({
 		varsion = false,
 	},
 })
+
+require("config.remaps")
+require("config.set")
+require("config.lsp")
+
